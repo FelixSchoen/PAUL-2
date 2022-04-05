fb219509f126ad0dedb1910c6ccae095e62dc07e
Felix Schön <mail@fschoen.com>
2022-04-05 13:40:47 +0200

    Update Trainer to be more performant

--- src/network/training.py
+++ src/network/training.py
@@ -6,28 +6,28 @@ from src.network.optimization import loss_function, accuracy_function
 class Trainer:
     signature = None
 
-    def __init__(self, transformer, optimizer, train_loss, train_accuracy, signature) -> None:
+    def __init__(self, transformer, optimizer, train_loss, train_accuracy) -> None:
         super().__init__()
         self.transformer = transformer
         self.optimizer = optimizer
         self.train_loss = train_loss
         self.train_accuracy = train_accuracy
-        Trainer.signature = signature
 
     def __call__(self, *args, **kwargs):
-        self.train_step(args[0], args[1])
+        self.train_step(self.transformer, self.optimizer, self.train_loss, self.train_accuracy, args[0], args[1])
 
-    @tf.function #(input_signature=signature)
-    def train_step(self, inp, tar):
+    @staticmethod
+    @tf.function
+    def train_step(transformer, optimizer, train_loss, train_accuracy, inp, tar):
         tar_inp = tar[:, :-1]
         tar_real = tar[:, 1:]
 
         with tf.GradientTape() as tape:
-            predictions, _ = self.transformer([inp, tar_inp], training=True)
+            predictions, _ = transformer([inp, tar_inp], training=True)
             loss = loss_function(tar_real, predictions)
 
-        gradients = tape.gradient(loss, self.transformer.trainable_variables)
-        self.optimizer.apply_gradients(zip(gradients, self.transformer.trainable_variables))
+        gradients = tape.gradient(loss, transformer.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, transformer.trainable_variables))
 
-        self.train_loss(loss)
-        self.train_accuracy(accuracy_function(tar_real, predictions))
+        train_loss(loss)
+        train_accuracy(accuracy_function(tar_real, predictions))
