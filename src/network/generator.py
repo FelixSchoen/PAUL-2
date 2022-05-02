377f88f346fc1adb8c462e19286e674e8860e944
Felix Schön <mail@fschoen.com>
2022-05-02 17:53:19 +0200

    Implement categorical sampling

--- src/network/generator.py
+++ src/network/generator.py
@@ -9,19 +9,30 @@ class Generator(tf.Module):
         super().__init__()
         self.transformer = transformer
 
-    def __call__(self, input_tensors):
+    def __call__(self, input_tensors, temperature=0):
         for input_tensor in input_tensors:
             assert isinstance(input_tensor, tf.Tensor)
 
-        output = [0 for _ in range(SEQUENCE_MAX_LENGTH - 1)]
-        output[0] = START_TOKEN
+        output = [START_TOKEN]
 
-        for i in range(1):
-            output_tensor = tf.convert_to_tensor(output, dtype=D_TYPE)
+        # Loop
+        for i in tf.range(SEQUENCE_MAX_LENGTH - 510):  # TODO
+            output_padded = output + [0] * (SEQUENCE_MAX_LENGTH - 1 - len(output))
+            output_tensor = tf.convert_to_tensor(output_padded, dtype=D_TYPE)
             output_tensor = tf.expand_dims(output_tensor, 0)
 
             predictions, _ = self.transformer([input_tensors, output_tensor], training=False)
-            prediction_tensor = predictions[0][i]  # TODO Different, use -1
-            prediction = tf.argmax(prediction_tensor)  # TODO Categorical sampling
+            prediction_tensor = predictions[:, i]  # TODO Different, uses -1
 
-            print(prediction)
+            if temperature == 0:
+                prediction = tf.argmax(prediction_tensor, axis=-1)
+            else:
+                prediction = tf.random.categorical(prediction_tensor / temperature, 1)[0]
+
+            output.append(prediction[0])
+
+        # Calculate attention
+        output_padded = output + [0] * (SEQUENCE_MAX_LENGTH - 1 - len(output))
+        output_tensor = tf.convert_to_tensor(output_padded, dtype=D_TYPE)
+        output_tensor = tf.expand_dims(output_tensor, 0)
+        _, attention_weights = self.transformer([input_tensors, output_tensor], training=False)
